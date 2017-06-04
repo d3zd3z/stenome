1fbe7af1a9ffd0d2de5f78ebee9b2a0d3119ade6
David Brown <davidb@davidb.org>
2017-06-04 01:55:18 -0600

    timelearn: Create initial timelearn crate
    The timelearn crate manages the problem storage for a time-based
    learning system.  It is agnostic to the meaning of the questions and
    answers (just that they are valid UTF-8), but manages time intervals.
    The data is stored in an sqlite3 database.
    
    There is a test that exercises the API, but doesn't perform testing that
    the operation is correct.

---
+++ timelearn/src/lib.rs
@@ -0,0 +1,182 @@
+/// srs: A model for spaced repetition system learning.
+///
+/// This crate maintains a database of problems, and some notions of intervals and a bit of history
+/// in order to allow them to be used for spaced repetition learning.
+
+extern crate rand;
+extern crate rusqlite;
+
+use rand::{Rng, thread_rng};
+use rusqlite::{Connection, Transaction};
+use std::error;
+use std::path::Path;
+use std::result;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+// For now, just box the errors.  TODO: Make a real error type.
+pub type Result<T> = result::Result<T, Box<error::Error + Send + Sync>>;
+
+/// A Store holds problems in a database (and holds the handle to the database).
+pub struct Store {
+    /// The connection to the database.
+    conn: Connection,
+}
+
+impl Store {
+    /// Create a new store at the given path.  Will return an error if the database has already
+    /// been created.
+    pub fn create<P: AsRef<Path>>(path: P) -> Result<Store> {
+        let mut conn = Connection::open(path)?;
+
+        {
+            let tx = conn.transaction()?;
+            tx.execute("CREATE TABLE probs (id INTEGER PRIMARY KEY,
+                question TEXT UNIQUE,
+                answer TEXT NOT NULL)", &[])?;
+            tx.execute("CREATE TABLE learning (probid INTEGER PRIMARY KEY REFERENCES probs (id),
+                next REAL NOT NULL,
+                interval REAL NOT NULL)", &[])?;
+            tx.execute("CREATE INDEX learning_next ON learning (next)", &[])?;
+            tx.execute("CREATE TABLE schema_version (version TEXT NOT NULL)", &[])?;
+            tx.execute("INSERT INTO schema_version VALUES (?)", &[&"20170408B"])?;
+            tx.commit()?;
+        }
+
+        conn.execute("PRAGMA foreign_keys = ON", &[])?;
+
+        Ok(Store {
+            conn: conn,
+        })
+    }
+
+    /// Return a populator that can be used to more rapidly populate the data.  The population will
+    /// be done within the context of a single sqlite3 database transaction.
+    pub fn populate(&mut self) -> Result<Populator> {
+        let tx = self.conn.transaction()?;
+        Ok(Populator {
+            tx: tx,
+        })
+    }
+
+    /// Query for the next problem that has expired.  If Some, then this is the next problem that
+    /// should be asked.
+    pub fn get_next(&mut self) -> Result<Option<Problem>> {
+        let mut stmt = self.conn.prepare("
+            SELECT id, question, answer, next, interval
+            FROM probs JOIN learning
+            WHERE probs.id = learning.probid
+                AND next <= ?
+            ORDER BY next
+            LIMIT 1")?;
+        let mut rows = stmt.query_map(&[&now()], |row| {
+            Problem {
+                id: row.get(0),
+                question: row.get(1),
+                answer: row.get(2),
+                next: row.get(3),
+                interval: row.get(4),
+            }
+        })?;
+        match rows.next() {
+            Some(row) => Ok(Some(row?)),
+            None => Ok(None),
+        }
+    }
+
+    /// Get a problem that hasn't started being learned.  The interval and "next" will be set
+    /// appropriately for a new word.
+    pub fn get_new(&mut self) -> Result<Option<Problem>> {
+        let mut stmt = self.conn.prepare("
+            SELECT id, question, answer
+            FROM probs
+            WHERE ID NOT IN (SELECT probid FROM learning)
+            ORDER BY id
+            LIMIT 1")?;
+        let mut rows = stmt.query_map(&[], |row| {
+            Problem {
+                id: row.get(0),
+                question: row.get(1),
+                answer: row.get(2),
+                next: now(),
+                interval: 5.0,
+            }
+        })?;
+        match rows.next() {
+            Some(row) => Ok(Some(row?)),
+            None => Ok(None),
+        }
+    }
+
+    /// Update a word, based on a learning factor.  The scale is 1..4, with 1 being totally
+    /// incorrect, and 4 being totally correct.
+    pub fn update(&mut self, prob: Problem, factor: u8) -> Result<()> {
+        let factor = match factor {
+            1 => 0.25,
+            2 => 0.9,
+            3 => 1.2,
+            4 => 2.2,
+            _ => panic!("Invalid factor: {}", factor),
+        };
+
+        let mut prob = prob;
+
+        let mut rng = thread_rng();
+        let interval = prob.interval;
+        // Compute the interval, capping with a minimum of 5 seconds.
+        prob.interval = (interval * factor * rng.gen_range(0.75, 1.25)).max(5.0);
+        prob.next = now() + prob.interval;
+
+        let tx = self.conn.transaction()?;
+        tx.execute("INSERT OR REPLACE INTO learning VALUES (?, ?, ?)",
+            &[&prob.id, &prob.next, &prob.interval])?;
+        tx.commit()?;
+
+        Ok(())
+    }
+}
+
+/// A single problem retrieved.
+pub struct Problem {
+    id: i64,
+    pub question: String,
+    pub answer: String,
+    next: f64,
+    interval: f64,
+}
+
+pub struct Populator<'a> {
+    tx: Transaction<'a>,
+}
+
+impl<'a> Populator<'a> {
+    /// Add a single unlearned problem to the store.
+    pub fn add_problem(&mut self, question: &str, answer: &str) -> Result<()> {
+        self.tx.execute("INSERT INTO probs (question, answer) VALUES (?, ?)",
+            &[&question, &answer])?;
+        Ok(())
+    }
+
+    /// Add a problem that is in the process of being learned.  The 'next' value is the unix time
+    /// that the question should be asked again, and 'interval' is the current interval.
+    pub fn add_learning_problem(&mut self, question: &str, answer: &str, next: f64, interval: f64) -> Result<()> {
+        self.tx.execute("INSERT INTO probs (question, answer) VALUES (?, ?)",
+            &[&question, &answer])?;
+        self.tx.execute("INSERT INTO learning VALUES (?, ?, ?)",
+            &[&self.tx.last_insert_rowid(), &next, &interval])?;
+        Ok(())
+    }
+
+    pub fn commit(self) -> Result<()> {
+        self.tx.commit()?;
+        Ok(())
+    }
+}
+
+// Get the current time as ticks.
+pub fn now() -> f64 {
+    let stamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let sec = stamp.as_secs();
+    let nsec = stamp.subsec_nanos();
+
+    sec as f64 + (nsec as f64 / 1.0e9)
+}
---
+++ timelearn/tests/learn.rs
@@ -0,0 +1,98 @@
+// Test the learning code.
+
+extern crate rand;
+extern crate tempdir;
+extern crate timelearn;
+
+use rand::{Rng, SeedableRng, XorShiftRng};
+use tempdir::TempDir;
+use timelearn::{now, Populator, Store, Result};
+use std::io::Write;
+
+#[test]
+fn learning() {
+    let tmp_dir = TempDir::new("learn").unwrap();
+    let db_path = tmp_dir.path().join("learn.db");
+    let mut st = Store::create(db_path).unwrap();
+
+    populate(&mut st).unwrap();
+
+    // Ask all of the problems that we can.
+    let mut num = 1;
+    loop {
+        let prob = match st.get_next().unwrap() {
+            None => break,
+            Some(p) => p,
+        };
+
+        println!("Ask {} ({})", prob.question, num);
+        st.update(prob, num).unwrap();
+        num = num % 4 + 1;
+    }
+
+    // Then ask all of the new problems.
+    loop {
+        let prob = match st.get_new().unwrap() {
+            None => break,
+            Some(p) => p,
+        };
+
+        println!("New {} ({})", prob.question, num);
+        st.update(prob, num).unwrap();
+        num = num % 4 + 1;
+    }
+
+    // The challenge here then is figuring out if this result is meaningful.
+    // TODO
+}
+
+// Populate with test data.  Make a mix of unlearned problems, and learned ones that are ready to
+// learn, and those that aren't ready to learn.
+fn populate(st: &mut Store) -> Result<()> {
+    let mut p = st.populate()?;
+    let cur = now();
+
+    // These are in the past, ready to ask.
+    for i in 1 .. 11 {
+        add_one(&mut p, i, Some((cur - 20.0, 5.0)))?;
+    }
+
+    // These are in the future.
+    for i in 11 .. 21 {
+        add_one(&mut p, i, Some((cur + 20.0, 5.0)))?;
+    }
+
+    // And these haven't been asked at all.
+    for i in 21 .. 31 {
+        add_one(&mut p, i, None)?;
+    }
+
+    p.commit()?;
+    Ok(())
+}
+
+// Add a problem of the given number.  Next and interval can optionally be set.
+fn add_one<'a>(pop: &mut Populator<'a>, num: i32, ni: Option<(f64, f64)>) -> Result<()> {
+    let mut rng: XorShiftRng = SeedableRng::from_seed([num as u32, 1, 2, 3]);
+
+    let mut qn = vec![];
+    write!(&mut qn, "qn{}:", num).unwrap();
+    for _ in 1 .. rng.gen_range(5, 40) {
+        qn.push(rng.gen_range(b'a', b'z'));
+    }
+
+    let mut an = vec![];
+    write!(&mut an, "an{}:", num).unwrap();
+    for _ in 1 .. rng.gen_range(5, 40) {
+        an.push(rng.gen_range(b'a', b'z'));
+    }
+
+    let qn = String::from_utf8(qn).unwrap();
+    let an = String::from_utf8(an).unwrap();
+    match ni {
+        None => pop.add_problem(&qn, &an)?,
+        Some((next, interval)) => pop.add_learning_problem(&qn, &an, next, interval)?,
+    }
+
+    Ok(())
+}
