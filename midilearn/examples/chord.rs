5ae047180993a98fdbd8cf44e48b09b23fbda1e4
David Brown <davidb@davidb.org>
2017-07-13 19:31:36 -0600

    Fix the description of the example chord
    Summary:
    The example chord's description doesn't match the expected chord, which
    makes it challenging to play that chord.  Fix this.
    
    Test Plan: Just review, it's just a textual change.
    
    Reviewers: d3zd3z
    
    Reviewed By: d3zd3z
    
    Differential Revision: https://davidb.phacility.com/D36

--- midilearn/examples/chord.rs
+++ midilearn/examples/chord.rs
@@ -5,7 +5,7 @@ extern crate midilearn;
 use midilearn::{MidiLearn, Problem, User};
 
 fn main() {
-    let prob = Problem::new("shell: Dm7-G7-CΔ (1/3-7)",
+    let prob = Problem::new("shell: Dm7-G7-CΔ (1/7-3)",
                             r#"{
                                 "type": "voicing",
                                 "chords": [[50,60,65],[43,59,65],[48,59,64]]
