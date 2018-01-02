fec55e7fe9f34ab5ea4eb54df8c945dc5ab46e70
David Brown <davidb@davidb.org>
2018-01-02 16:03:59 -0700

    Use map literal instead of init func

--- learn/ui.go
+++ learn/ui.go
@@ -15,12 +15,8 @@ type UI interface {
 
 // Mapping holding all of the UIs that can be learned.  The mapping
 // holds a generator to generate the UI.
-var allUI map[string]func() (UI, error)
-
-func init() {
-	allUI = make(map[string]func() (UI, error))
-
-	Register("simple", newSimpleUI)
+var allUI = map[string]func() (UI, error){
+	"simple": newSimpleUI,
 }
 
 // Register adds a new user interface.  Any database with the given
